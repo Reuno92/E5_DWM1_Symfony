515246512636dbc390c46d6a4d19d41bfe26480e
Renaud Racinet <renaud.racinet@estiam.com>
2019-09-23 10:46:43 +0200

    feat(mainController): mainController created ! Test http request with a simple response
    no ticket or issue

--- src/Controller/mainController.php
+++ src/Controller/mainController.php
@@ -2,9 +2,17 @@
 
 
 namespace App\Controller;
-
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class mainController
 {
-
+    /**
+     * @Route("/", name="home")
+     * @return Response
+     */
+    public function index()
+    {
+        return new Response("Response !");
+    }
 }
